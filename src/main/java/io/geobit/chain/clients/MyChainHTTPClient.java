011aa3e988d49910b72222972e24a24230e110ec
Riccardo Casatta <riccardo.casatta@gmail.com>
2014-11-19 15:22:06 +0100

    MyChain mock up

--- src/main/java/io/geobit/chain/clients/MyChainHTTPClient.java
+++ src/main/java/io/geobit/chain/clients/MyChainHTTPClient.java
@@ -1,9 +1,32 @@
 package io.geobit.chain.clients;
 
-public class MyChainHTTPClient {
+import io.geobit.chain.providers.balance.BalanceProvider;
+import io.geobit.chain.providers.transaction.TransactionProvider;
+import io.geobit.common.entity.Transaction;
+
+public class MyChainHTTPClient implements BalanceProvider, TransactionProvider {
+
+	@Override
+	public String getPrefix() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
 	/**
 	 * http://mychain.io/api/tx?txid=d6d3ba8e88d7abc4aa61f313fc403786a0d53c27dc8dcb9d5cf3304fb627ae70
 	 * 
 	 */
+	@Override
+	public Transaction getTransaction(String txHash) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Long getBalance(String address) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 
 }
